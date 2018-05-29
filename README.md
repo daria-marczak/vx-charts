808eb6afd9b7f8e853e2c3dd1e156e081dd00d8c
daria marczak <marczak.ria@gmail.com>
2018-05-29 12:40:36 +0200

    Checking pie charts

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Series of exercises with data presentation in React
 
-[x] Line chart /25/05/2018
+- [x] Line chart /25/05/2018
+- [x] Pie chart from npm package /29/05/2018
+- [x] Donut chart from npm package /29/05/2018
--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
 import LineChart from "./LineChart";
+import PieChart from "react-minimal-pie-chart";
+import DonutChart from "react-donut-chart";
 
 const App = () => {
   return (
-    <LineChart />
+    <React.Fragment>
+      <LineChart />
+      <PieChart 
+        data={[
+          { value: 10, color: "#E38627"},
+          { value: 15, color: "#C13C27"},
+          { value: 20, color: "#6A2135"},
+        ]}
+      />
+      <DonutChart
+        data={[{
+          label: "Give you up",
+          value: 25
+        },
+        {
+          label: "",
+          value: 75,
+          isEmpty: true
+        }]} 
+      />
+    </React.Fragment>
   )
 }
 
